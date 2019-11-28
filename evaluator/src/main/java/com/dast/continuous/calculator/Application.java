e320bd9cf9997250a48ce1d1a78a112c8bf68308
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2019-11-28 13:37:03 +0100

    Subimos base de la aplicación Evaluator

---
+++ evaluator/src/main/java/com/dast/continuous/calculator/Application.java
@@ -0,0 +1,17 @@
+package com.dast.continuous.calculator;
+
+/**
+ * Inicializador de spring boot
+ * 
+ * 
+ * @author jorge
+ *
+ */
+public class Application {
+	
+    public static void main( String[] args ) {
+    	
+    	System.out.println("Iniciado");
+    	
+    }
+}
